0740a0f95de0cd2e189c5ab2133cdc192c04710c
MrPowers <matthewkevinpowers@gmail.com>
2016-01-27 11:49:47 -0500

    Require methods defined in Turf::Local to also be in Turf::Default.

--- lib/turf/lookup.rb
+++ lib/turf/lookup.rb
@@ -1,14 +1,22 @@
 module Turf; class Lookup
 
   def find(message)
+    if local_class && default_class
+      message = "The following methods are defined in the #{local_class} class but not the #{default_class} class: #{methods_in_local_and_not_default.join(', ')}.  All methods defined in #{local_class} must also be defined in #{default_class}."
+      raise message unless methods_in_local_and_not_default.empty?
+    end
+    raise "No Turf classes found... these must be defined and required" if classes.empty?
     lookup_path.each do |obj|
       return obj.send(message) if obj.respond_to?(message)
     end
-    raise "No Turf classes found... these must be defined and required" if classes.empty?
     raise NoMethodError, "The #{message} method could not be found in any of these Turf configuration classes: #{classes.join(", ")}"
   end
 
   private
+
+  def methods_in_local_and_not_default
+    local_class.instance_methods(false) - default_class.instance_methods(false)
+  end
 
   def lookup_path
     classes.map(&:new)
